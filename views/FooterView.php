d1955c2856c318babc9d20b2b753c0b3c42966ba
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-12-10 00:12:48 -0800

    Create footer.php

---
+++ views/FooterView.php
@@ -0,0 +1,10 @@
+<footer class="page-footer font-small  h-25">
+
+
+    <!-- Copyright -->
+    <div class="footer-copyright bg-warning text-center py-3 mt-3  " style="font-size: 20px;font-family: raleway;"> All rights © reserved by Secret-Menus 2020
+
+    </div>
+    <!-- Copyright -->
+
+</footer>
