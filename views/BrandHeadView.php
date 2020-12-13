d328917cb53a4e1121682a0b2b724928255ec939
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-12-13 00:01:58 -0800

    Create BrandHeadView.php

---
+++ views/BrandHeadView.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-light bg-warning justify-content-around mb-5">
+    <a class="navbar-brand" href="index.php">
+        <img src="imgs/logo.png" width="80">
+    </a>
+    <div class="form-inline">
+        <h2><?= $this->title ?></h2>
+    </div>
+    <a href="index.php" class="btn bg-dark text-light" style="font-size: 15px;font-family: raleway;"> Back</a>
+</nav>
+<div class="clearfix"></div>
