f528991c16a5d38801e8d4509f5bfbbd24a2db0b
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-12-13 00:01:45 -0800

    Create BrandFormView.php

---
+++ views/BrandFormView.php
@@ -0,0 +1,32 @@
+<div class="row p-5 ">
+    <div class="col col-md-8 offset-md-2 bg-light">
+        <form method="post" enctype="multipart/form-data" action="index.php?action=save" class="my-5">
+            <div class="form-group">
+                <label for="name" style="font-size: 20px;font-family: Vollkorn;">Brand Name </label>
+                <input name="name" type="text" class="form-control" id="name" placeholder="Missing Brand Name">
+            </div>
+
+            <div class="form-group">
+                <label for="category" style="font-size: 20px;font-family: Vollkorn;">Category</label>
+                <select name="category" class="form-control" id="category">
+                    <?php
+                    $categories = CategoryModel::list();
+
+                    foreach ($categories as $category) {
+                    ?>
+                        <option value="<?= $category->id ?>"><?= $category->name ?></option>
+                    <?php
+                    }
+
+                    ?>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="image" style="font-size: 20px;font-family: Vollkorn;">Image</label>
+                <input name="image" type="file" class="form-control-file" id="image">
+            </div>
+
+            <button type="submit" class="btn btn-info mb-2" style="font-family: Vollkorn;">Submit</button>
+        </form>
+    </div>
+</div>
