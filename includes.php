bb75c401a1422293b74f2f980c450252c294133f
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-01-14 12:50:03 -0800

    New Update new Include  for ads

--- includes.php
+++ includes.php
@@ -1,6 +1,8 @@
 <?php
 
 include_once("./models/DatabaseModel.php");
+include_once("./models/AdFactory.php");
+include_once("./models/AdModel.php");
 include_once("./models/BrandFactory.php");
 include_once("./models/BrandModel.php");
 include_once("./models/SecretMenuFactory.php");
