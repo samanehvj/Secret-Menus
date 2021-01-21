5b9483a27c8b538f148e4a11c8e3721b0695feb5
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-01-21 00:38:42 -0800

    Create BrandView.php

--- views/BrandsView.php
+++ views/BrandsView.php
@@ -4,12 +4,27 @@
         <?php
         foreach ($this->data as $brand) {
         ?>
-            <div class="card d-inline-block">
+            <div class="card d-inline-block mb-4 ">
                 <img class="card-img-top" src="imgs/<?= $brand->image ?>" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title"><?= $brand->name ?></h5>
 
-                    <a href="index.php?action=view&id=<?= $brand->id ?>" class="btn bg-info text-light btn-block" style="font-size: 20px;font-family: Vollkorn;">View</a>
+                    <a href="index.php?action=view&id=<?= $brand->id ?>" class="btn-dark text-light w-25 btn-lg text-center ml-5 " style="font-size: 20px;font-family: ovo;">View</a>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
+
+        <?php
+        foreach ($this->ads as $ad) {
+        ?>
+            <div class="card d-inline-block ">
+                <img class="card-img-top" src="<?= $ad->img ?>" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $ad->name ?></h5>
+
+                    <a href="<?= $ad->link ?>" target="_blank" class="btn-dark text-light w-25 btn-lg text-center ml-5 ">Shop</a>
                 </div>
             </div>
         <?php
