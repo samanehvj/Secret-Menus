f7fc02c71051be8862c1c10eb75c27637db1bcc2
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-02-02 01:05:37 -0800

    Update MainView.php

--- views/MainView.php
+++ views/MainView.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
     <title><?= $this->title ?> Secret Menus</title>
 </head>
 
