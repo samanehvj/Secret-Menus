66f99ad0b6fdf63d1102c95755a35e0b52e756fa
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-03-03 16:44:04 -0800

    Update BrandsView.php

--- views/BrandsView.php
+++ views/BrandsView.php
@@ -1,4 +1,18 @@
 <div class="home">
+<!-- <p class="intro">20 restaurant are available in this  website </p> -->
+
+<h1 class="ml14">
+  <span class="text-wrapper">
+    <span class="letters">15 restaurants are available in this  website</span>
+    <span class="line"></span>
+  </span>
+</h1>
+<div class="text-center">
+  <h2 class="">Do you have new restaurants that should be added</h2> 
+  <button type="button" class="btn btn-warning text-dark mb-4">
+  <a class="text-dark font-weight-bold" href="#form">Add Here</a>
+  </button>
+  </div>
     <div class="card-columns">
 
         <?php
@@ -23,7 +37,6 @@
                 <img class="card-img-top" src="<?= $ad->img ?>" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title"><?= $ad->name ?></h5>
-
                     <a href="<?= $ad->link ?>" target="_blank" class="btn-dark text-light w-25 btn-lg text-center ml-5 ">Shop</a>
                 </div>
             </div>
@@ -33,3 +46,35 @@
 
     </div>
 </div>
+
+<script>
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml14 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml14 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeInOutExpo",
+    duration: 900
+  }).add({
+    targets: '.ml14 .letter',
+    opacity: [0,1],
+    translateX: [40,0],
+    translateZ: 0,
+    scaleX: [0.3, 1],
+    easing: "easeOutExpo",
+    duration: 800,
+    offset: '-=600',
+    delay: (el, i) => 150 + 25 * i
+  }).add({
+    targets: '.ml14',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+</script>
