59ade92f6271b1faa81a1d44d21055388249ca0c
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-03-03 16:43:46 -0800

    Update BrandHeadView.php

--- views/BrandHeadView.php
+++ views/BrandHeadView.php
@@ -2,9 +2,10 @@
     <a class="navbar-brand" href="index.php">
         <img src="imgs/logo.png" width="130">
     </a>
-    <div class="form-inline">
-        <h2><?= $this->title ?></h2>
-    </div>
+
     <a href="index.php" class="btn bg-dark text-light" style="font-size: 15px;font-family: ovo;"> Back</a>
 </nav>
-<div class="clearfix"></div>
+<!-- <div class="clearfix"></div>
+    <div class="form-inline">
+        <h2 class="text-center"><?= $this->title ?></h2>
+    </div> -->
